488fcee16953ac03f8237b27265c8ad9170d4ece
João Canavarro <jvcanavarro@gmail.com>
2021-01-29 17:48:18 -0300

    Implement Bresenham

--- sketch.js
+++ sketch.js
@@ -6,26 +6,29 @@ class Bresenham {
         this.y1 = coords[1][1];
     }
 
-    drawLine () {
-        var dx = Math.abs(this.x1 - this.x0);
-        var dy = Math.abs(this.y1 - this.y0);
-        var sx = (this.x0 < this.x1) ? 1 : -1;
-        var sy = (this.y0 < this.y1) ? 1 : -1;
-        var err = dx - dy;
-        
+    drawLine() {
+        var dx = abs(this.x1 - this.x0);
+        var sx = this.x0 < this.x1 ? 1 : -1;
+        var dy = -abs(this.y1 - this.y0)
+        var sy = this.y0 < this.y1 ? 1 : -1;
+        var err = dx + dy;
+        var i = 0;
         while(true) {
-            setPixel(this.x0, this.y0);
-        
-            if ((this.x0 === x1) && (this.y0 === y1)) break;
-            var e2 = 2*err;
+            color[this.x0 * w + this.y0] = !color[this.x0 * w + this.y0];
+
+            if (this.x0 == this.x1 && this.y0 == this.y1)
+            break;
             
-            if (e2 > -dy) { 
-                err -= dy; 
-                x0  += sx; }
-            
-            if (e2 < dx) { 
-                err += dx; 
-                y0  += sy; }
+            var e2 = 2 * err;
+
+            if (e2 >= dy) {
+            err += dy;
+            this.x0 += sx;
+            }
+            if (e2 <= dx) {
+            err += dx;
+            this.y0 += sy;
+            }
         }
     }
 }
@@ -43,18 +46,22 @@ var bresenham;
 
 function setup() {
     createCanvas(1500, 980);
-    for (var i = 0; i < 30; i++) {
+
+    for (var i = 0; i < w; i++) {
         x[i] = w + i * w;
         y[i] = w + i * w;
     }
+    
+    clearGrid();
 
-    for (var i = 0; i < 900; i++) {
-        color[i] = true;
-    }
-
+    // Refactor
     bres_button = createButton('Bresenham');
-    bres_button.position(1000, 19);
+    bres_button.position(1000, 20);
     bres_button.mousePressed(startBresenham);
+
+    clear_button = createButton('Clear');
+    clear_button.position(1000, 70);
+    clear_button.mousePressed(clearGrid);
 }
 
 function startBresenham(){
@@ -62,27 +69,33 @@ function startBresenham(){
     bresenham.drawLine();
 }
 
+// Grid might become a class
+function clearGrid() {
+    for (var i = 0; i < w**2; i++) {
+        color[i] = true;
+    }
+}
+
 function draw() {
     background(255);
     rectMode(CENTER);
     stroke(0);
     for (var i = 0; i < y.length; i++) {
-        for (var j = 0; j < 30; j++) {
-            if (color[i * 30 + j]) fill("white");
+        for (var j = 0; j < w; j++) {
+            if (color[i * w + j]) fill("white");
             else fill("pink");
             rect(x[j], y[i], w, w);
         }
     }
 }
 function mousePressed() {
-    for (var i = 0; i < 30; i++) {
-        for (var j = 0; j < 30; j++) {
+    for (var i = 0; i < w; i++) {
+        for (var j = 0; j < w; j++) {
             var dis = dist(mouseX, mouseY, x[j], y[i]);
             if (dis < w/2){
-                color[i * 30 + j] =! color[i * 30 + j];
+                color[i * w + j] = !color[i * w + j];
                 coords.push([i, j]);
             }
         }
     }
 }
-
