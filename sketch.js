a199c64d3c625f35d5b5f0a28fa304670878f4ba
Aian Shay <aianshay@gmail.com>
2021-02-10 15:08:31 -0300

    Add circle

--- sketch.js
+++ sketch.js
@@ -13,24 +13,72 @@ class Bresenham {
         var sy = this.y0 < this.y1 ? 1 : -1;
         var err = dx + dy;
         var i = 0;
+        
+        paint(this.x1, this.y1);
         while(true) {
-            color[this.x0 * w + this.y0] = !color[this.x0 * w + this.y0];
-
             if (this.x0 == this.x1 && this.y0 == this.y1)
-            break;
+                break;
             
             var e2 = 2 * err;
 
             if (e2 >= dy) {
-            err += dy;
-            this.x0 += sx;
+                err += dy;
+                this.x0 += sx;
             }
             if (e2 <= dx) {
-            err += dx;
-            this.y0 += sy;
+                err += dx;
+                this.y0 += sy;
             }
+            paint(this.x0, this.y0);
         }
     }
+}
+
+class Circle {
+    constructor (coords, radius) {
+        this.x0 = coords[0][0];
+        this.y0 = coords[0][1];
+        this.p = 3 - (2 * radius);
+        this.x = 0;
+        this.y = radius;
+        color[this.x0 * w + this.y0] = true
+    }
+
+    drawCircle() {
+        this.paintPixel(this.x0, this.y0, this.x, this.y);
+
+        while (this.x < this.y)
+        {
+            this.x++;
+            if (this.p < 0)
+            {
+                this.p += (4 * this.x) + 6;
+            }
+            else
+            {
+                this.y--;
+                this.p += 4 * (this.x - this.y) + 10;
+            }
+            this.paintPixel(this.x0, this.y0, this.x, this.y);
+        }
+    }
+
+    paintPixel(xc, yc, x, y) {
+        paint(xc + x, yc + y); 
+        paint(xc - x, yc + y); 
+        paint(xc + x, yc - y); 
+        paint(xc - x, yc - y); 
+
+        paint(xc + y, yc + x); 
+        paint(xc - y, yc + x); 
+        paint(xc + y, yc - x); 
+        paint(xc - y, yc - x); 
+    }
+}
+
+function paint(x, y) {
+    if (color[x * w + y])
+        color[x * w + y] = false;
 }
 
 var x = [];
@@ -42,10 +90,9 @@ var start = [];
 var end = [];
 let coords = [];
 
-var bresenham;
 
 function setup() {
-    createCanvas(1500, 980);
+    createCanvas(1500, 1000);
 
     for (var i = 0; i < w; i++) {
         x[i] = w + i * w;
@@ -59,14 +106,31 @@ function setup() {
     bres_button.position(1000, 20);
     bres_button.mousePressed(startBresenham);
 
+    circle_title = createElement('h4', 'Circle');
+    circle_title.position(1000, 40);
+    circle_subtext = createElement('h5', 'Radius');
+    circle_subtext.position(1000, 60);
+    circle_input = createInput();
+    circle_input.position(1000, 100);
+    circle_input.size(70);
+    circle_button = createButton('Draw');
+    circle_button.position(1080, 100);
+    circle_button.mousePressed(startCircle);
+    
     clear_button = createButton('Clear');
-    clear_button.position(1000, 70);
+    clear_button.position(1000, 800);
     clear_button.mousePressed(clearGrid);
 }
 
-function startBresenham(){
+function startBresenham() {
     bresenham = new Bresenham(coords);
     bresenham.drawLine();
+}
+
+function startCircle(radius) {
+    radius = circle_input.value()
+    circle = new Circle(coords, parseInt(radius, 10));
+    circle.drawCircle();
 }
 
 // Grid might become a class
@@ -74,6 +138,7 @@ function clearGrid() {
     for (var i = 0; i < w**2; i++) {
         color[i] = true;
     }
+    coords = []
 }
 
 function draw() {
@@ -88,6 +153,7 @@ function draw() {
         }
     }
 }
+
 function mousePressed() {
     for (var i = 0; i < w; i++) {
         for (var j = 0; j < w; j++) {
