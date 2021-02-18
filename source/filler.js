fb3e5a75c0f519eb31aef5b5e921b84ff868ab08
João Canavarro <jvcanavarro@gmail.com>
2021-02-18 20:24:59 -0300

    Rename check color to CamelCase

--- source/filler.js
+++ source/filler.js
@@ -1,6 +1,6 @@
 class Filler {
     flood(x, y) {
-        if (check_color(x, y)) {
+        if (getColor(x, y)) {
             paint(x, y);
             this.flood(x + 1, y);
             this.flood(x, y + 1);
